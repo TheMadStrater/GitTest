daee7a69f53c30e2fc1b5aa9661da58cf4eb5a58
Shannon Richards <shannonr@magenic.com>
2015-11-11 10:48:31 -0500

    Small modifications

--- GitTestConsoleApp/GitTestConsoleApp/Program.cs
+++ GitTestConsoleApp/GitTestConsoleApp/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace GitTestConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Adding a new comment
         }
     }
 }
