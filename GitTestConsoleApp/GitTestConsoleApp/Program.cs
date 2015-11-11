098b04303291f181d164bf6336784ecb258a3f80
Shannon Richards <shannonr@magenic.com>
2015-11-11 11:04:18 -0500

    Latest v1.1 bits

--- GitTestConsoleApp/GitTestConsoleApp/Program.cs
+++ GitTestConsoleApp/GitTestConsoleApp/Program.cs
@@ -10,6 +10,7 @@ namespace GitTestConsoleApp
             // Adding another comment
             // Adding yet another comment
 
+            // Adding a new comment v1.1
             var items = new List<Item>();
         }
     }
